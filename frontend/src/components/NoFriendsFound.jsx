e01ab3b1d92d6f52ca5f1ed3221188675797f3d9
GautamxKumar_209 <siddharthgtm236@gmail.com>
2025-08-24 11:06:34 +0530

    New-Quote

--- frontend/src/components/NoFriendsFound.jsx
+++ frontend/src/components/NoFriendsFound.jsx
@@ -3,7 +3,7 @@ const NoFriendsFound = () => {
     <div className="card bg-base-200 p-6 text-center">
       <h3 className="font-semibold text-lg mb-2">No friends yet</h3>
       <p className="text-base-content opacity-70">
-        Connect with language partners below to start practicing together!
+       Connect with partners, Chat openly, Share experiences, Learn something new every day
       </p>
     </div>
   );
